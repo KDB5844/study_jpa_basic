826dfb105ccafb06e7e77027a4c0ad9f105bf3f2
Subin Cho <sbjo7713@gmail.com>
2022-05-24 02:15:38 +0900

    값 타입 컬렉션 사용 방법 및 수정 코드 그로 인한 대안

---
+++ ex-hello-jpa/src/main/java/hellojpa/collection/CollectionExam.java
@@ -0,0 +1,61 @@
+package hellojpa.collection;
+
+import hellojpa.Member;
+import hellojpa.embedded.Address;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class CollectionExam {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUserName("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("START======================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // Collection 은 기본값이 LAZY Loading
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
+
+            System.out.println("END======================");
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
---
+++ ex-hello-jpa/src/main/java/hellojpa/collection/UpdateExam.java
@@ -0,0 +1,71 @@
+package hellojpa.collection;
+
+import hellojpa.Member;
+import hellojpa.embedded.Address;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
+
+public class UpdateExam {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUserName("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("START======================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // homeCity -> newCity
+            Address newAddress = new Address("newCity", findMember.getHomeAddress().getStreet(), findMember.getHomeAddress().getZipcode());
+            findMember.setHomeAddress(newAddress);
+
+            // 치킨 -> 한식
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            favoriteFoods.remove("치킨");
+            favoriteFoods.add("한식");
+
+            // old1 -> new1
+            List<Address> addressList = findMember.getAddressHistory();
+
+            addressList.remove(new Address("old1", "street", "10000"));
+            addressList.add(new Address("new1", "street", "10000"));
+
+            System.out.println("END======================");
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
